32f832191222a053345b7a9c5c23f934d95ed07e
nagisa599 <nagi46460510@icloud.com>
2024-03-02 17:06:10 +0900

    test

--- lib/main.dart
+++ lib/main.dart
@@ -35,7 +35,7 @@ class MyApp extends StatelessWidget {
       ),
       builder: DevicePreview.appBuilder,
       locale: DevicePreview.locale(context),
-      home: const TopPage(title: 'Flutter Demo Home Pageff'),
+      home: const TopPage(title: 'Flutter Demo Home Pafgeff'),
     );
   }
 }
